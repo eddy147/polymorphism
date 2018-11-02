7000f510ec25a6edc0a3113fdb58a1d04f46ceee
eddy.de.boer <eddy147@gmail.com>
2018-11-02 15:11:29 +0100

    Added main

--- src/polymorphism/core.clj
+++ src/polymorphism/core.clj
@@ -6,7 +6,15 @@
 (defmethod encounter [:Lion :Bunny] [l b] :eat)
 (defmethod encounter [:Lion :Lion] [l1 l2] :fight)
 (defmethod encounter [:Bunny :Bunny] [b1 b2] :mate)
+
 (def b1 {:Species :Bunny :other :stuff})
 (def b2 {:Species :Bunny :other :stuff})
 (def l1 {:Species :Lion :other :stuff})
 (def l2 {:Species :Lion :other :stuff})
+
+(defn -main []
+  (do
+    (println (encounter b1 l1))
+    (println (encounter b1 b2))
+    (println (encounter l1 l2))
+    (println (encounter l1 b1))))
